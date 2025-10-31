86d3803670506ebf5eda235dc28a5cfff2178017
Scaffolder <scaffolder@backstage.io>
2025-10-31 14:58:10 +0530

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  name: 'mynewnextapp',
+  description: 'nextjs new component',
+  owner: 'group:default/guests',
+  repoOwner: 'shubhamvjv',
+  repoName: 'nextazurereponew',
+  azureWebAppName: 'mynewnextapp',
+  azureResourceGroup: 'coe-platform-engineering-rg',
+  azureRegion: 'southeastasia',
+}
